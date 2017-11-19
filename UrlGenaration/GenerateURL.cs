47fa55f123f23ab507179e97bfa16b87309d9e80
yuvarani23 <yuvarani23@yahoo.com>
2017-11-19 18:02:30 -0500

    hgjsjsj

--- UrlGenaration/GenerateURL.cs
+++ UrlGenaration/GenerateURL.cs
@@ -6,5 +6,28 @@ namespace UrlGenaration
 {
     class GenerateURL
     {
+        public string BrowseNode { get; set; }
+        public string ItemPage { get; set; }
+
+        public string GetURL()
+        {
+            string url = string.Empty;
+            try
+            {
+                Authentication param = new Authentication() { AccessKey = "AKIAJ3PYT5Y23QKLK6OA", SecretKey = "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" };
+                Sign sign = new Sign(param);
+                ParamURL operation = new ParamURL();
+                operation.AssociateTag("project2k17-20");
+                operation.BrowseNode(BrowseNode);
+                operation.ItemPage(ItemPage);
+                operation.Operation("ItemSearch");
+                operation.ResponseGroup("ItemAttributes,Large");
+                operation.SearchIndex("Appliances");
+                url = sign.SignURL(operation);
+
+            }
+            catch (Exception e) { e.ToString(); if (string.IsNullOrEmpty(url) == true) Console.WriteLine("Returning Empty String"); }
+            return url;
+        }
     }
 }
